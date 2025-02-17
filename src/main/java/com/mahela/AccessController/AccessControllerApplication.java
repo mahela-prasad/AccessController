690a3d90eeaa1ec169123800db8dcf7a9b11497d
Mahela Prasad <84281002+mahela-prasad@users.noreply.github.com>
2025-02-17 08:01:27 +0000

    first

---
+++ src/main/java/com/mahela/AccessController/AccessControllerApplication.java
@@ -0,0 +1,13 @@
+package com.mahela.AccessController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahela/AccessController/AccessControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahela.AccessController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
